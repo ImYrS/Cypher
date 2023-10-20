24491383cf294b652f9905d1abfa897735f87590
ImYrS <i@imyrs.com>
2023-10-21 00:58:47 +0800

    feat: supported worker link

--- static/js/header.js
+++ static/js/header.js
@@ -1,10 +1,11 @@
 const initPath = () => {
   let path = location.pathname.replace('.html', '');
   let apiToken = localStorage.apiToken;
+  let accountId = localStorage.accountId;
 
-  if (path === "/login" && apiToken) {
+  if (path === "/login" && apiToken && accountId) {
     location.href = "/zones.html";
-  } else if ((path === "/zones" || path === "/zone") && !apiToken) {
+  } else if ((path === "/zones" || path === "/zone") && (!apiToken || !accountId)) {
     location.href = "/login.html";
   }
 };
